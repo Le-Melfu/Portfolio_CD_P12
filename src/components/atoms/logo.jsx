684b1c5cdb58744125e7440c9e624a6e7ea74b74
Melfu <degardenzi.clement@gmail.com>
2024-05-02 17:12:24 +0200

    Logo size optimisation

--- src/components/atoms/logo.jsx
+++ src/components/atoms/logo.jsx
@@ -1,7 +1,12 @@
-import logo from '../../assets/icons/logo-clem.png'
+import { useMediaQuery } from 'react-responsive'
+import logoDesktop from '../../assets/icons/logo-clem.webp'
+import logoMobile from '../../assets/icons/logo-clem-mobile.webp'
 import './logo.scss'
 
 const Logo = () => {
+    const isMobile = useMediaQuery({ maxWidth: 769 })
+    const logo = isMobile ? logoMobile : logoDesktop
+
     return (
         <>
             <img src={logo} alt="Logo de Clément Degardenzi" className="logo" />
