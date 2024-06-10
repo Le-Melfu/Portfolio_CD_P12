62317a31e8fb34b2d1fcc77ffd5e4d17b5dda73b
Melfu <degardenzi.clement@gmail.com>
2024-06-10 12:31:18 +0200

    Added intégrateur web to formations

--- src/pages/experiencePage.jsx
+++ src/pages/experiencePage.jsx
@@ -13,6 +13,7 @@ const ExperiencePage = () => {
                 <ScrolledBanner bannerNum="1">
                     <h3>Formations et diplômes</h3>
                     <ul>
+                        <li>Intégrateur web - OpenClassrooms</li>
                         <li>
                             Musicien Interprète des Musiques Actuelles - EF2M
                             Tourcoing
