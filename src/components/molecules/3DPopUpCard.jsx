eff8efe0c656db0edbde7fcc1731c2f6031fafb0
Melfu <degardenzi.clement@gmail.com>
2024-09-16 15:38:38 +0200

    btn target blank link removed

--- src/components/molecules/3DPopUpCard.jsx
+++ src/components/molecules/3DPopUpCard.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './3DPopUpCard.scss'
 import { useContext } from 'react'
 import { ThemeContext } from '../../assets/ThemeContext'
