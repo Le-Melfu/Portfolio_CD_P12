1cd6f84fc3643e692c4f3e2f6993d26ad8363b7f
Melfu <degardenzi.clement@gmail.com>
2024-09-27 15:37:53 +0200

    première itération

--- src/components/organisms/header.jsx
+++ src/components/organisms/header.jsx
@@ -5,6 +5,8 @@ import NavbarMobile from '../molecules/navbar-mobile'
 import { useContext } from 'react'
 import { ThemeContext } from '../../assets/ThemeContext'
 import MaltBtn from '../atoms/maltBtn'
+import './header.scss'
+import backgroundGif from '../../assets/videos/3.gif'
 
 const Header = () => {
     const isMobile = useMediaQuery({ maxWidth: 769 })
@@ -22,6 +24,12 @@ const Header = () => {
 
             {isMobile ? <NavbarMobile /> : <Navbar />}
             <MaltBtn />
+            <div className="background-animation background-animation--left">
+                <img src={backgroundGif} alt="Animation"></img>
+            </div>
+            <div className="background-animation background-animation--right">
+                <img src={backgroundGif} alt="Animation"></img>
+            </div>
         </header>
     )
 }
