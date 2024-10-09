8bf565f46bf6fced0acd68404353c59140dc3422
Melfu <degardenzi.clement@gmail.com>
2024-10-09 16:16:21 +0200

    working and sound fx ok

--- src/pages/gamePage.jsx
+++ src/pages/gamePage.jsx
@@ -4,19 +4,41 @@ import { ThemeContext } from '../assets/ThemeContext'
 import spaceshipAsset from '../assets/images/gamePageAssets/vecteezy_spaceship-3d-rendering-icon-illustration_28567053.png'
 import Projectile from '../components/atoms/gameProjectile'
 import Enemy from '../components/atoms/gameEnnemy'
+import shootMP3 from '../assets/music/shoot.mp3'
+import hitMP3 from '../assets/music/hit.mp3'
 
 const GamePage = () => {
     const { isDark } = useContext(ThemeContext)
+    const [hitSound, setHitSound] = useState(null)
+
     const [inputs, setInputs] = useState({
         left: false,
         right: false,
         up: false,
         down: false,
     })
+    const gameWidth = 1440
+    const gameHeight = window.innerHeight
+
+    const [isGameRunning, setIsGameRunning] = useState(false)
+    const [playerLost, setPlayerLost] = useState(false)
+    const startGame = () => {
+        setPlayerLost(false)
+        setIsGameRunning(true)
+        setEnemies([]) // Réinitialiser les ennemis
+        setProjectiles([]) // Réinitialiser les projectiles
+        setPlayerPosition({
+            x: gameWidth / 2 - 25,
+            y: gameHeight / 2 + 150,
+        })
+        setHealthPoints(3)
+    }
+    // État pour les points de vie
+    const [healthPoints, setHealthPoints] = useState(3)
 
     const [playerPosition, setPlayerPosition] = useState({
-        x: window.innerWidth / 2 - 25,
-        y: window.innerHeight / 2 - 25,
+        x: gameWidth / 2 - 25,
+        y: gameWidth / 2 - 25,
     })
 
     const speed = 10
@@ -32,15 +54,20 @@ const GamePage = () => {
     ]
     const [enemies, setEnemies] = useState([])
     const checkCollision = (proj, enemy) => {
-        console.log(
-            `Projectile: (${proj.x}, ${proj.y}), Enemy: (${enemy.x}, ${enemy.y})`
-        )
-        return (
-            proj.x < enemy.x + 50 && // Largeur de l'ennemi
-            proj.x + 4 > enemy.x && // Largeur du projectile
-            proj.y < enemy.y + 50 && // Hauteur de l'ennemi
-            proj.y + 4 > enemy.y // Hauteur du projectile
-        )
+        const isCollision =
+            proj.x < enemy.x + 50 &&
+            proj.x + 4 > enemy.x &&
+            proj.y < enemy.y + 50 &&
+            proj.y + 4 > enemy.y
+        if (isCollision) {
+            hitSound.currentTime = 0 // Réinitialise le son pour le rejouer
+            hitSound
+                .play()
+                .catch((error) =>
+                    console.error('Erreur de lecture du son:', error)
+                )
+        }
+        return isCollision
     }
 
     // Gérer les inputs de mouvement
@@ -83,19 +110,38 @@ const GamePage = () => {
             }
         }
 
-        window.addEventListener('keydown', handleKeyDown)
-        window.addEventListener('keyup', handleKeyUp)
+        if (isGameRunning) {
+            window.addEventListener('keydown', handleKeyDown)
+            window.addEventListener('keyup', handleKeyUp)
+        }
 
         return () => {
             window.removeEventListener('keydown', handleKeyDown)
             window.removeEventListener('keyup', handleKeyUp)
         }
-    }, [])
+    }, [isGameRunning])
 
+    useEffect(() => {
+        const sound2 = new Audio(hitMP3)
+
+        setHitSound(sound2)
+
+        return () => {
+            // Optionnel : Libération des ressources audio si nécessaire
+
+            sound2.pause()
+        }
+    }, [])
     // Gérer l'entrée de tir séparément
     useEffect(() => {
         const handleSpaceDown = (event) => {
             if (event.code === 'Space' && canShoot) {
+                const laserSound = new Audio(shootMP3)
+                laserSound
+                    .play()
+                    .catch((error) =>
+                        console.error('Erreur de lecture du son:', error)
+                    )
                 setCanShoot(false)
                 setProjectiles((prevProjectiles) => [
                     ...prevProjectiles,
@@ -108,13 +154,13 @@ const GamePage = () => {
                 setTimeout(() => setCanShoot(true), 50) // Délai de 300 ms
             }
         }
-
-        window.addEventListener('keydown', handleSpaceDown)
-
+        if (isGameRunning) {
+            window.addEventListener('keydown', handleSpaceDown)
+        }
         return () => {
             window.removeEventListener('keydown', handleSpaceDown)
         }
-    }, [canShoot, playerPosition.x, playerPosition.y])
+    }, [canShoot, playerPosition.x, playerPosition.y, isGameRunning])
 
     // Mettre à jour la position du joueur
     useEffect(() => {
@@ -127,6 +173,11 @@ const GamePage = () => {
                 if (inputs.right) newX += speed
                 if (inputs.up) newY -= speed
                 if (inputs.down) newY += speed
+
+                // Limiter la position X
+                newX = Math.max(0, Math.min(newX, gameWidth - 50)) // 50 pour la largeur du joueur
+                // Limiter la position Y
+                newY = Math.max(0, Math.min(newY, gameHeight - 50)) // 50 pour la hauteur du joueur
 
                 return { x: newX, y: newY }
             })
@@ -151,6 +202,7 @@ const GamePage = () => {
                         const hit = updatedProjectiles.some((proj) =>
                             checkCollision(proj, enemy)
                         )
+
                         return !hit // Retire les ennemis touchés
                     })
                 })
@@ -162,27 +214,36 @@ const GamePage = () => {
         const animationFrameId = requestAnimationFrame(update)
 
         return () => cancelAnimationFrame(animationFrameId)
-    }, [projectiles])
+    }, [projectiles, isGameRunning])
 
     // Gérer les ennemis
     useEffect(() => {
         const spawnEnemy = () => {
             const randomChallenge =
                 challenges[Math.floor(Math.random() * challenges.length)]
-            const enemyX = Math.random() * (window.innerWidth - 100) // Position X aléatoire
-            const direction = Math.random() < 0.5 ? 1 : -1 // Direction initiale
+            const enemyX = Math.random() * (gameWidth - 50) // Position X aléatoire
+            const enemySpeed = Math.random() * 2 + 1
 
             setEnemies((prevEnemies) => [
                 ...prevEnemies,
-                { x: enemyX, y: 0, type: randomChallenge, direction }, // Ajout de la direction
+                {
+                    x: enemyX,
+                    y: 0,
+                    type: randomChallenge,
+                    speed: enemySpeed,
+                }, // Ajout de la direction
             ])
         }
 
-        const enemyInterval = setInterval(spawnEnemy, 3000) // Générer un ennemi toutes les 3 secondes
+        const enemyInterval = setInterval(() => {
+            if (isGameRunning) {
+                spawnEnemy()
+            }
+        }, 2000) // Générer un ennemi toutes les 3 secondes // Générer un ennemi toutes les 3 secondes
 
         return () => clearInterval(enemyInterval)
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, [isGameRunning])
 
     // Mettre à jour la position des ennemis
     useEffect(() => {
@@ -194,17 +255,44 @@ const GamePage = () => {
 
                         return {
                             ...enemy,
-                            y: enemy.y + 2, // Descendre l'ennemi
+                            y: enemy.y + enemy.speed, // Descendre l'ennemi
                         }
                     })
                     .filter((enemy) => enemy.y < window.innerHeight) // Garder les ennemis à l'écran
             })
         }
 
-        const enemyMovementInterval = setInterval(moveEnemies, 16)
+        const enemyMovementInterval = setInterval(() => {
+            if (isGameRunning) {
+                moveEnemies()
 
+                // Vérifier si un ennemi touche le bas de l'écran
+                setEnemies((prevEnemies) => {
+                    const updatedEnemies = prevEnemies.filter((enemy) => {
+                        if (enemy.y >= window.innerHeight - 50) {
+                            // Si l'ennemi touche le bas de l'écran, diminuer les points de vie
+                            setHealthPoints((prevHealth) => {
+                                const newHealth = prevHealth - 1
+                                if (newHealth <= 0) {
+                                    // Logique si les points de vie atteignent zéro (fin du jeu par exemple)
+                                    setPlayerLost(true) // Alerte pour le Game Over
+                                    setIsGameRunning(false) // Arrête le jeu
+                                    return 0 // Pour éviter d'avoir des points de vie négatifs
+                                }
+
+                                return newHealth
+                            })
+
+                            return false // Retirer l'ennemi
+                        }
+                        return true // Garder l'ennemi
+                    })
+                    return updatedEnemies
+                })
+            }
+        }, 16)
         return () => clearInterval(enemyMovementInterval)
-    }, [])
+    }, [isGameRunning])
 
     const changeEnemyDirection = (enemy) => {
         return {
@@ -223,22 +311,48 @@ const GamePage = () => {
 
     return (
         <main className={`game page fade-in ${isDark ? '' : 'light'}`}>
-            <div
-                className="player"
-                style={{
-                    left: `${playerPosition.x}px`,
-                    top: `${playerPosition.y}px`,
-                }}
-            >
-                <img src={spaceshipAsset} alt="vaisseau spatial" />
-            </div>
+            {!isGameRunning &&
+                !playerLost && ( // Écran de démarrage
+                    <div className="start-screen">
+                        <button onClick={startGame}>Start</button>
+                    </div>
+                )}
+            {playerLost && ( // Écran de Game Over
+                <div className="game-over-screen">
+                    <h2>Game Over</h2>
+                    <h3>Points de vie restants: {healthPoints}</h3>
+                    <button onClick={startGame}>Rejouer</button>
+                </div>
+            )}
+            {isGameRunning &&
+                !playerLost && ( // Zone de jeu active
+                    <>
+                        <div
+                            className="player"
+                            style={{
+                                left: `${playerPosition.x}px`,
+                                top: `${playerPosition.y}px`,
+                            }}
+                        >
+                            <img src={spaceshipAsset} alt="vaisseau spatial" />
+                        </div>
 
-            {projectiles.map((proj, index) => (
-                <Projectile key={index} x={proj.x} y={proj.y} />
-            ))}
-            {enemies.map((enemy, index) => (
-                <Enemy key={index} x={enemy.x} y={enemy.y} type={enemy.type} />
-            ))}
+                        {projectiles.map((proj, index) => (
+                            <Projectile key={index} x={proj.x} y={proj.y} />
+                        ))}
+                        {enemies.map((enemy, index) => (
+                            <Enemy
+                                key={index}
+                                x={enemy.x}
+                                y={enemy.y}
+                                type={enemy.type}
+                            />
+                        ))}
+                        <div className="health">
+                            <h2>Health Points: {healthPoints}</h2>
+                        </div>
+                    </>
+                )}
         </main>
     )
 }
